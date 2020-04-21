2999d7f114a38f3a8bf522095a83701353794d93
Marvinlee93 <53844282+Marvinlee93@users.noreply.github.com>
2020-04-21 20:59:25 +0800

    added events

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ app.get('/session', function(req, res, next) {
   Parse incoming events - configure your Session Monitoring url in TKBX dashboard... Inspect the events!
   */
   app.get('/events', function(req, res, next) {
-    console.log(JSON.stringify(req.body));
+    console.log("EVENT CALLBACK: ",JSON.stringify(req.body));
+    res.json({ incomingevents: JSON.stringify(req.body) })
   });
   
   
