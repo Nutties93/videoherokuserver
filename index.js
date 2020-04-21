aad9485200fe45b5bef879d9b2402b8cdc0228c2
Marvinlee93 <53844282+Marvinlee93@users.noreply.github.com>
2020-04-21 18:22:25 +0800

    added session

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+  
+
+// This value can be stored in an ecrypted place like Azure Key Vault.
+var  apiKey = '46688592';
+const apiSecret = 'af3d6b6c800d665f6a56f8e36474a5714f59220c';
+
+var OpenTok = require('opentok'),
+    opentok = new OpenTok(apiKey, apiSecret);
+
+
+// Set up webhook endpoint for front end to call! 
+// Creates a new session when called! Front end needs to check if there's an existing sessionId. T
+// This can be done through cookies encryption, react context or redux state management
+app.get('/session', function(req, res, next) {
+    var sessionId = "";
+    var token = " "
+    // Create a session
+    opentok.createSession(function(err, session) {
+    if (err) return console.log(err);
+        sessionId = session.sessionId;
+
+        //session.generateToken() needs to be inside of opentok! Initializing it as a new obj throws in an error
+        token = session.generateToken({
+          role :                   'publisher',
+          expireTime :             (new Date().getTime() / 1000)+(1 * 60 * 60), // 1 day
+          data :                   'name=Johnny',
+          initialLayoutClassList : ['focus']
+        });
+
+      /* Create token and return all required values to client  */
+      if (token) {
+        res.json({ currentToken: token, currentSessionId: sessionId, apiKey:  apiKey });
+      } else {
+          console.log(" Error occurred when generating token using session Id")
+          res.json({ currentToken: "", currentsessionId: ""  })
+      }
+    });
+  });
+
+  
+  
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
